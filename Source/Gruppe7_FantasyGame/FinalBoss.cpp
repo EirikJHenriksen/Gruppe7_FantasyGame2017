62ecb34d4e1435956d6575dba812c71153fb8b72
Eirik J. Henriksen <eirikjhen@gmail.com>
2017-04-23 16:54:51 +0200

    Made the finalBoss class. Made some changes to last level. Boss teleports around in room.

---
+++ Source/Gruppe7_FantasyGame/FinalBoss.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gruppe7_FantasyGame.h"
+#include "FinalBoss.h"
+#include "MagicProjectile.h"
+#include "ConeOfFire.h"
+#include "CircleOfThorns.h"
+#include "PhysAttackBox.h"
+#include "Gruppe7_FantasyGameCharacter.h"
+#include "FantasyGameInstance.h"
+
+
+// Sets default values
+AFinalBoss::AFinalBoss()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	GetCapsuleComponent()->bGenerateOverlapEvents = true;
+	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &AFinalBoss::OnOverlap);
+}
+
+// Called when the game starts or when spawned
+void AFinalBoss::BeginPlay()
+{
+	Super::BeginPlay();
+
+	fightInProgress = true; // SKAL VÆRE FALSE!
+
+	canTeleport = true;
+}
+
+// Called every frame
+void AFinalBoss::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (fightInProgress && canTeleport)
+	{	
+		canTeleport = false;
+
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Blue, TEXT("TIMER - ACTIVE"));
+
+		RandomTeleportTime = FMath::RandRange(RandomMin, RandomMax);
+
+		GetWorldTimerManager().SetTimer(TeleportTimerHandle, this, &AFinalBoss::Teleport, RandomTeleportTime, false);
+
+		//GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Yellow, TEXT("TELEPORTER - FUNCTION OVER"));
+	}
+}
+
+// Called to bind functionality to input
+void AFinalBoss::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AFinalBoss::Teleport()
+{	
+	GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Yellow, TEXT("TELEPORTER - FUNCTION IN PROGRESS"));
+
+	// Teleports the boss to one of three random spots.
+	int random = FMath::RandRange(0, 2);
+
+	// FIKS SÅ HAN IKKE TELEPORTERER TIL SAMME STED SOM HAN STÅR!
+
+	switch (random)
+	{
+	default:
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Red, TEXT("SOMETHING WENT WRONG WITH BOSS TELEPORT! FUNCTION: Teleport()"));
+		break;
+	case 0:
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Yellow, TEXT("TELEPORT - FIRE"));
+		//UGameplayStatics::PlaySoundAtLocation(GetWorld(), SFX01, GetActorLocation());
+		//VFX.
+		SetActorLocation(FVector(FireX, FireY, FireZ), false);
+		break;
+	case 1:
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Yellow, TEXT("TELEPORT - WATER"));
+		//UGameplayStatics::PlaySoundAtLocation(GetWorld(), SFX02, GetActorLocation());
+		//VFX.
+		SetActorLocation(FVector(WaterX, WaterY, WaterZ), false);
+		break;
+	case 2:
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Yellow, TEXT("TELEPORT - NATURE"));
+		//UGameplayStatics::PlaySoundAtLocation(GetWorld(), SFX03, GetActorLocation());
+		//VFX.
+		SetActorLocation(FVector(NatureX, NatureY, NatureZ), false);
+		break;
+	}
+
+	GetWorld()->GetTimerManager().ClearTimer(TeleportTimerHandle);
+
+	canTeleport = true;
+}
+
+void AFinalBoss::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	// Physic attack
+	if (OtherActor->IsA(APhysAttackBox::StaticClass()))
+	{
+		OtherActor->Destroy();
+
+	}
+
+	// Magic Projectile - WATER
+	if (OtherActor->IsA(AMagicProjectile::StaticClass()))
+	{
+		OtherActor->Destroy();
+
+		if (!fightInProgress)
+		{
+			//Spill av partikkeleffekt og lyd. - Onskapsfull latter som viser at angrepet ikke gjør noe.
+		}
+
+	}
+
+	// Circle of thorns - NATURE
+	if (OtherActor->IsA(ACircleOfThorns::StaticClass()))
+	{
+
+	}
+
+	// Cone of fire - FIRE
+	if (OtherActor->IsA(AConeOfFire::StaticClass()))
+	{
+
+	}
+}
---
+++ Source/Gruppe7_FantasyGame/FinalBoss.h
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "FinalBoss.generated.h"
+
+UCLASS()
+class GRUPPE7_FANTASYGAME_API AFinalBoss : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFinalBoss();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/////////////////////////////////////////
+	// TELEPORTATION VARIABLES.
+	void Teleport();
+
+	bool fightInProgress;
+
+	bool canTeleport;
+
+	FTimerHandle TeleportTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport timers")
+		float RandomMin = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport timers")
+		float RandomMax = 10;
+
+	float RandomTeleportTime = 0;
+
+	// Teleport locations.
+	
+	// FIRE
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport FIRE")
+		float FireX = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport FIRE")
+		float FireY = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport FIRE")
+		float FireZ = 0;
+
+	// WATER
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport WATER")
+		float WaterX = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport WATER")
+		float WaterY = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport WATER")
+		float WaterZ = 0;
+
+	// NATURE
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport NATURE")
+		float NatureX = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport NATURE")
+		float NatureY = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleport NATURE")
+		float NatureZ = 0;
+
+	// Overlap function
+	UFUNCTION()
+		void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+	
+};
